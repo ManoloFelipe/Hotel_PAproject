b16126ccb1d591c66d914e775df7716660f8004a
ManoloFelipe <manolofelipec74@gmail.com>
2025-05-15 22:06:41 -0600

    test footer 2

--- footer/footer.js
+++ footer/footer.js
@@ -3,7 +3,10 @@ window.addEventListener('DOMContentLoaded', () => {
     const footerContainer = document.getElementById('footer-container');
     if (footerContainer) {
         fetch('./footer/footer.html')
-            .then(response => response.text())
+            .then(response => {
+                if (response.ok) return response.text();
+                throw new Error('Footer not found');
+            })
             .then(data => {
                 footerContainer.innerHTML = data;
             })
